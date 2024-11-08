f5a40d7395bd7e924a46827530c7d287acc19a78
Francesco <inbox@francescorocca.me>
2024-11-08 16:38:18 +0100

    v0.2 ready

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # 📥 YTDLBot - Telegram Video Downloader
 
 ## 📦 Requirements
-Before getting started, make sure you have the following installed:
+Before getting started, ensure you have the following installed:
+
 - [uv](https://github.com/astral-sh/uv)
 - [telegram-bot-api](https://github.com/tdlib/telegram-bot-api)
 - [ffmpeg](https://www.ffmpeg.org/download.html)
@@ -9,28 +10,37 @@ Before getting started, make sure you have the following installed:
 ## 🚀 Quick Start Guide
 
 ### Step 1: Run Your Local Bot API Server
-Follow the instructions to set up your local server from the [official documentation](https://tdlib.github.io/telegram-bot-api/build.html).
+Follow the instructions to set up your local server from the [official documentation](https://tdlib.github.io/telegram-bot-api/build.html). If you don't want to build it yourself, you can check tdlib.native.
 
 ### Step 2: Obtain API Credentials
 Get your `api-id` and `api-hash` from the [Telegram API](https://core.telegram.org/api/obtaining_api_id).
 
-### Step 3: Set Up Environment Variables
-Set your environment variables with your obtained credentials:
-
-```bash
-export TELEGRAM_API_ID=YOUR_API_ID
-export TELEGRAM_API_HASH=YOUR_API_HASH
-```
-
-### Step 4: Start the Server
+### Step 3: Start the Server
 Run the bot API server using the following command:
 
 ```bash
-telegram-bot-api --api-id YOUR_API_ID --api-hash YOUR_API_HASH --http-port 7575
+telegram-bot-api --api-id YOUR_API_ID --api-hash YOUR_API_HASH --http-port 7575 --local
+```
+
+### Step 4: Log Out the Bot from the Official API
+Open this link in your browser:
+
+```url
+https://api.telegram.org/bot<YOUR_TOKEN>/logOut
+```
+
+Make sure to replace `<YOUR_TOKEN>` with your actual bot token. You should see a JSON response that looks like this:
+
+```json
+{"ok":true,"result":true}
 ```
 
 ### Step 5: Interact with Your Bot
-Start chatting with your bot on Telegram and begin downloading videos!
+Rename `.env-example` to `.env` and add your bot token there (or pass it to the script with `-t <TOKEN>`). Then run the bot:
+
+```bash
+uv run bot.py
+```
 
 ## 🤝 Contributing
 We welcome contributions from the community! Feel free to submit issues, fork the project, and create pull requests.
--- bot.py
+++ bot.py
@@ -11,10 +11,24 @@ from dotenv import load_dotenv
 import os
 import argparse
 import validators
+import ffmpeg
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Hello {update.effective_user.first_name}!")
+
+
+def get_video_metadata(filename):
+    probe = ffmpeg.probe(filename)
+    video_stream = next(
+        (stream for stream in probe["streams"] if stream["codec_type"] == "video"), None
+    )
+    if video_stream:
+        width = int(video_stream["width"])
+        height = int(video_stream["height"])
+        duration = int(float(probe["format"]["duration"]))
+        return width, height, duration
+    return None, None, None
 
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -23,8 +37,6 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("Please provide a valid URL!")
         return
 
-    await update.message.reply_text(f"Downloading {url}...")
-
     ytdlp_opts = {
         "format": "bestvideo+bestaudio/best",
         "outtmpl": "./videos/%(title)s.%(ext)s",
@@ -32,26 +44,49 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     }
 
     with YoutubeDL(ytdlp_opts) as ytdl:
+        update_msg = await update.message.reply_text(f"Downloading {url}...")
         info_dict = ytdl.extract_info(url, download=True)
         filename = ytdl.prepare_filename(info_dict)
 
-        video_duration = info_dict.get("duration", 0)
-        video_width = info_dict.get("width", 0)
-        video_height = info_dict.get("height", 0)
-        thumbnail = info_dict.get("thumbnail", None)
+        video_width, video_height, video_duration = get_video_metadata(filename)
+        video_title = info_dict.get("title", "No title found")
 
-        await context.bot.send_message(
-            chat_id=update.message.chat_id, text="Uploading video..."
+        thumbnail_file = "./videos/thumb.jpg"
+        (
+            ffmpeg.input(filename, ss=5)
+            .filter("scale", video_width, -1)
+            .output(thumbnail_file, vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
         )
 
-        media = InputMediaVideo(
-            media=open(filename, "rb"),
-            width=video_width,
-            height=video_height,
-            duration=video_duration,
-            thumbnail=thumbnail,
+        await context.bot.edit_message_text(
+            text="Uploading...",
+            chat_id=update.message.chat_id,
+            message_id=update_msg.id,
         )
-        await update.message.reply_media_group(media=[media])
+
+        with open(filename, "rb") as video_file:
+            media = InputMediaVideo(
+                media=video_file,
+                width=video_width,
+                height=video_height,
+                duration=video_duration,
+                thumbnail=open(thumbnail_file, "rb") if thumbnail_file else None,
+                supports_streaming=True,
+            )
+            await update.message.reply_media_group(
+                media=[media],
+                caption=f"{video_title}\n\n{url}\nInfo: {video_width}x{video_height}",
+            )
+
+            await context.bot.delete_message(
+                chat_id=update_msg.chat_id, message_id=update_msg.id
+            )
+
+    os.remove(filename)
+    if thumbnail_file:
+        os.remove(thumbnail_file)
 
 
 def main():
@@ -59,15 +94,25 @@ def main():
         prog="YTdlBot", description="Telegram Video Downloader Bot"
     )
     parser.add_argument("-t", "--token", help="Bot token")
-    parser.add_argument("-w", "--webhook", help="Webhook URL")
     args = parser.parse_args()
-    print(args)
 
-    print("Loading bot token...")
-    load_dotenv()
-    bot_token = os.getenv("BOT_TOKEN")
+    if args.token:
+        print("Token obtained from the args...")
+        bot_token = args.token
+    else:
+        print("Loading env vars...")
+        load_dotenv()
+        bot_token = os.getenv("BOT_TOKEN")
 
-    app = ApplicationBuilder().token(bot_token).build()
+    app = (
+        ApplicationBuilder()
+        .token(bot_token)
+        .base_url("http://localhost:7575/bot")
+        .local_mode(True)
+        .read_timeout(20)
+        .write_timeout(35)
+        .build()
+    )
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(~filters.COMMAND, message))
     app.run_polling()
